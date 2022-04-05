8c72794d1bf2d6738f5ae62a32a5053cfdcb48fa
sherwinlobo06 <61878383+sherwinlobo06@users.noreply.github.com>
2022-04-05 19:42:06 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -331,32 +331,3 @@ if option == 'Research':
     if screen == 'Chart':
         work_in_progress()
 
-        
-# IEX
-class IEXStock:
-
-    def __init__(self, token, symbol, environment='production'):
-        if environment == 'production':
-            self.BASE_URL = 'https://cloud.iexapis.com/v1'
-        else:
-            self.BASE_URL = 'https://sandbox.iexapis.com/v1'
-        
-        self.token = token
-        self.symbol = symbol
-
-    def get_logo(self):
-        url = f"{self.BASE_URL}/stock/{self.symbol}/logo?token={self.token}"
-        r = requests.get(url)
-
-        return r.json()
-
-    def get_company_info(self):
-        url = f"{self.BASE_URL}/stock/{self.symbol}/company?token={self.token}"
-        r = requests.get(url)
-
-        return r.json()    
-    def get_company_news(self, last=10):
-        url = f"{self.BASE_URL}/stock/{self.symbol}/news/last/{last}?token={self.token}"
-        r = requests.get(url)
-
-        return r.json()

222c6e8980db7d54393c8690e2423cd391cbc2e7
sherwinlobo06 <61878383+sherwinlobo06@users.noreply.github.com>
2022-04-21 11:49:19 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -83,7 +83,8 @@ def topgainer(df):
  assets = assets.set_index(pd.DatetimeIndex(assets['Date'].values))
  assets.drop(['Date'],inplace = True, axis=1)
  assets.dropna(axis=1, inplace=True)
- assets
+ 
+ st.dataframe(assets)
  daily_simple_returns = assets.pct_change()
  daily_simple_returns
  annualized_returns = daily_simple_returns.mean()*252

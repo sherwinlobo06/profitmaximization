19977871140c969d0bb6a7a59b18561a38537bb5
sherwinlobo06 <61878383+sherwinlobo06@users.noreply.github.com>
2022-04-21 12:14:41 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -66,7 +66,7 @@ def topgainer(df):
    
 
 #working
-option = st.sidebar.selectbox("Which Dashboard?", ('My Portfolio', 'Investment'), 1)
+option = st.sidebar.selectbox("Which Dashboard?", ('Investment',''), 1)
 manual_stock = []
 st.header(option)
 

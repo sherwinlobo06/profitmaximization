b4dbcd88c5d54c4373e9699fde596c7f82620975
sherwinlobo06 <61878383+sherwinlobo06@users.noreply.github.com>
2022-04-21 11:44:17 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -89,7 +89,7 @@ def topgainer(df):
  annualized_returns = daily_simple_returns.mean()*252
  annualized_returns
  sorted_annualized_returns = annualized_returns.sort_values(ascending=False)
- sorted_annualized_returns
+ st.write(sorted_annualized_returns)
  chart_data = pd.DataFrame(annualized_returns, annualized_returns.index)
  st.bar_chart(chart_data)
  chart_data = pd.DataFrame(sorted_annualized_returns, sorted_annualized_returns.index)
